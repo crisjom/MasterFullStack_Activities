887e61e03f600fb7e09232072f3d82cd2adc0dcd
crisjom <85122657+crisjom@users.noreply.github.com>
2023-06-16 13:30:15 +0200

    Update productsTable.js

--- Act4/script/productsTable.js
+++ Act4/script/productsTable.js
@@ -20,8 +20,11 @@ class ProductsTable {
     setTableRows() {
         const productsTable = document.getElementById('product-table-id');
         this.#products.forEach(product => {
+            const addProductToHTML = `<td class="product-table__cell"><h2>$(product.tittle)</h2><p>Ref: $(product.SKU)</p></td>`;
+            const addProductToHTML = `<td class="product-table__cell--input"><button onclick="decrement()">-</button><input type="number" id="quantity" value="1"><button onclick="increment()">+</button></td>`;
             const tableRow = document.createElement('tr');
             tableRow.id = product.SKU;
+            //tableRow.innerHTML = 
             productsTable.appendChild(tableRow);
         });
     }
